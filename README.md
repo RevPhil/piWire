d26ad76cbe8022dfc69b424ad68bc49e4054c9a6
Reverend Phil <37200388+RevPhil@users.noreply.github.com>
2020-11-15 11:39:29 +0000

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
 # piWire
  A simple emulation of the Arduino Wire library for the Raspberry Pi
+
+piWire is a simple header file which provides a basic emulation of the Arduino 'Wire' library for the Raspberry Pi.
+
+The maximum Transmission length is 32 Bytes.
+
+The default object is 'Wire'.
+
+The following 'Master' commands are supported:
+
+	int begin(void);
+	void beginTransmission(int targetI2c);
+	void write(uint8_t data);
+	void write(void *buf,int numBytes);
+	int endTransmission(void);
+	int requestFrom(int targetI2c, int numBytes);
+	int read(void);
+	int read(void *buf, int numBytes);
+	
+**int begin(void)**
+
+*Wire.begin();*
+
+Opens the 'i2c-1' port for reading and writing.
+
+*returns* the filehandle if successful or an error number if unsuccessful.
+
+'perror()' can be used to determine the error.
+
+*if(Wire.begin() < 0) perror("begin");*
+
+**void beginTransmission(int targetI2c)**
+
+Prepares the I2C buffer for transmission to the 'target' I2C address.
+
+*Wire.beginTransmission(0x55);*
+
+*returns* nothing.
+
+**void write(uint8_t data)**
+
+Writes single unsigned Bytes sequentially to the TX buffer (maximum 32 Bytes).
+
+*uint8_t data = 0xAA;*_
+
+*Wire.write(data);*
+
+*returns* nothing.
+
+**void write(void *buf,int numBytes)**
+
+Writes multiple Bytes from a variable or array etc. to the buffer (maximum 32 Bytes).
+
+*float source = 12345.12345;*
+
+*Wire.write(&source,sizeof(source));*
+
+*returns* nothing.
+
+**int endTransmission(void)**
+
+Closes the TX buffer and sends the buffer contents to the I2C address specified in 'beginTransmission()'.
+
+*Wire.endTransmission();*
+
+*returns* true if successful or an error number if unsuccessful.
+
+'perror()' can be used to determine the error.
+
+*if(Wire.endTransmission() < 0) perror("endTransmission");*
+
+**int requestFrom(int targetI2c, int numBytes)**
+
+Requests the number of Bytes specified from the I2C address (maximum 32 Bytes).
+
+*Wire.requestFrom(0x55,4);*
+
+*returns* the number of Bytes received if successful or an error number if unsuccessful.
+
+'perror()' can be used to determine the error.
+
+*if(Wire.requestFrom(0x55,4) < 0) perror("requestfrom");*
+
+**int read(void)**
+
+Reads single sequential Bytes from the RX buffer.
+
+*uint8_t myByte = Wire.read();*
+
+*returns* the Byte read or '-1' if the buffer end has been reached.
+
+*if(Wire.read() < 0) printf("read failed!");*
+
+**int read(void *buf, int numBytes)**
+
+Reads multiple Bytes from the RX buffer into a variable or array etc.
+
+*float data = 0.0;*
+
+*Wire.read(&data,sizeof(data));*
+
+*returns* the number of Bytes read or '-1' if the buffer end has been reached.
+
+*if(Wire.read(&data,sizeof(data)) < 0) printf("read failed!");*
---
+++ Wire.h
@@ -0,0 +1,129 @@
+#ifndef PIWIRE_H
+#define PIWIRE_H
+
+/*
+  Wire.h for Raspberry Pi
+  
+  Copyright (c) 2019 Reverend Phil Morris.  All rights reserved.
+  
+  A very simple emulation of the Arduino Wire library for Raspberry Pi use
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA  
+*/
+
+#include <iostream>
+#include <unistd.h>				//Needed for I2C port
+#include <fcntl.h>				//Needed for I2C port
+#include <sys/ioctl.h>			//Needed for I2C port
+#include <linux/i2c-dev.h>		//Needed for I2C port
+#include <cerrno>
+
+#define I2C_BUFFER_SIZE 32
+
+class piWire {
+public:
+	int begin(void);
+	void beginTransmission(int targetI2c);
+	void write(uint8_t data);
+	void write(void *buf,int numBytes);
+	int endTransmission(void);
+	int requestFrom(int targetI2c, int numBytes);
+	int read(void);
+	int read(void *buf, int numBytes);
+	
+	uint8_t buffer[(I2C_BUFFER_SIZE + 1)];
+private:
+	int file_i2c;
+	bool transmitting = false;
+	uint8_t bufferPointer = 0;
+	int currentI2c = -1;
+};
+
+int piWire::begin(void) {
+	//----- OPEN THE I2C-1 BUS -----
+	char *filename = (char*)"/dev/i2c-1";
+	file_i2c = open(filename, O_RDWR);
+	return file_i2c;
+}
+
+// SEND //
+// prepare to send data to Slave
+void piWire::beginTransmission(int targetI2c) {
+	bufferPointer = 0;
+	transmitting = true;
+	currentI2c = targetI2c;
+}
+
+// write a single Byte to the Slave (buffer)
+void piWire::write(uint8_t data) {
+	if(bufferPointer < I2C_BUFFER_SIZE && transmitting) buffer[bufferPointer++] = data;
+}
+
+// write an array or variable to the Slave (buffer)
+void piWire::write(void *buf,int len) {
+	if(len > I2C_BUFFER_SIZE) len = I2C_BUFFER_SIZE;
+	uint8_t* txBytes = reinterpret_cast<uint8_t*>(buf);
+	while(len--) {
+		write(*txBytes++);
+	}
+}
+
+// send the buffered data to the Slave and end transmission
+int piWire::endTransmission(void) {
+	if(!transmitting) return -1;
+	transmitting = false;
+	int result;
+	if ((result = ioctl(file_i2c, I2C_SLAVE, currentI2c)) < 0) return result;
+	if(bufferPointer > I2C_BUFFER_SIZE) bufferPointer = I2C_BUFFER_SIZE;
+	result = ::write(file_i2c, &buffer, bufferPointer);
+	bufferPointer = 0;
+	return result;
+}
+
+
+// RECEIVE //
+// fetch the given number of Bytes from the Slave into the buffer
+int piWire::requestFrom(int targetI2c, int numBytes) {
+	bufferPointer = 0;
+	int result;
+	if(numBytes > I2C_BUFFER_SIZE) numBytes = I2C_BUFFER_SIZE;
+	if ((result = ioctl(file_i2c, I2C_SLAVE, targetI2c)) < 0) return result;
+	result = ::read(file_i2c, buffer, numBytes);
+	if(result != numBytes)	return -1;
+	bufferPointer = 0;
+	return result;
+}
+
+// read a single Byte from the buffer
+int piWire::read(void) {
+	if(bufferPointer < I2C_BUFFER_SIZE)
+	return buffer[bufferPointer++];
+	else
+	return -1;
+}
+
+// read an array or variable from the buffer
+int piWire::read(void *buf, int numBytes) {
+	if(numBytes > I2C_BUFFER_SIZE) return -1;
+	uint8_t* rxBytes = reinterpret_cast<uint8_t*>(buf);
+	while(numBytes--) {
+		*rxBytes++ = read();
+	}
+	return numBytes;
+}
+
+piWire Wire;	// default object
+
+#endif
